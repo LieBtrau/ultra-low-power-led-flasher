9f2c0742fa92f828724b3d4b028caae03b220092
Christoph Tack <prog.send@gmail.com>
2020-04-24 22:30:42 +0200

    First writes to RV-3028-C7 are OK

--- Firmware/UltraLowPowerLedFlasher/lib/TWI_master/TWI_master.h
+++ Firmware/UltraLowPowerLedFlasher/lib/TWI_master/TWI_master.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <Arduino.h>
 /*! \brief Definition of pin used as SCL. */
-const byte SCL = PB3;
+const byte SCL = PB3; //SOIC8 : pin 2
 
 /*! \brief Definition of pin used as SDA. */
-const byte SDA = PB4;
+const byte SDA = PB4; //SOIC8 : pin 3
 
 /*! \brief Definition of PORT used as SCL. */
 #define PORT_SCL PORTB
@@ -19,15 +19,6 @@ const byte SDA = PB4;
 /*! \brief Definition of PIN used as SDA. */
 #define PIN_SDA PINB
 
-#define SET_SDA_OUT() DDR_SDA |= (1 << SDA)
-#define SET_SDA_IN() DDR_SDA &= ~(1 << SDA)
-
-/*! \brief Delay used to generate clock */
-#define DELAY 2
-
-/*! \brief Delay used for STOP condition */
-#define SCL_SDA_DELAY 1
-
 class TwiWire
 {
 public:
@@ -38,8 +29,6 @@ public:
     bool readRequestFrom(byte slaveAddress, byte *data, char bytes);
 
 private:
-    const byte WRITE = 0;
-    const byte READ = 1;
     byte slaveAddress; /*! \brief Slave 8 bit address (bits 7 down 1). */
     bool i2c_write_byte(bool send_start, bool send_stop, byte data);
     bool i2c_read_byte(bool nack, bool send_stop, byte& data);
--- Firmware/UltraLowPowerLedFlasher/src/main.cpp
+++ Firmware/UltraLowPowerLedFlasher/src/main.cpp
@@ -1,21 +1,47 @@
 #include <Arduino.h>
 #include "TWI_master.h"
 
-const byte VEML7700_ADDRESS = 0x20;
+static bool writeRegister(byte reg, byte value);
+static bool readRegister(byte reg, byte &value);
+const byte RV3028_ADDR = 0xA4;
+const byte REG_CTRL1 = 0x0F;
+const byte EERD = 3;
 TwiWire wire;
-byte dataIn[2];
-byte dataOut[1] = {0x00};
+
 
 void setup()
 {
   wire.begin();
-  wire.beginTransmission(VEML7700_ADDRESS);
-  if (wire.writeEndTransmission(dataOut, 1, false))
+  //automatic refresh function has to be disabled (EERD = 1): register 0Fh, bit 3
+  if(!writeRegister(REG_CTRL1, 1<<EERD))
   {
-    wire.readRequestFrom(VEML7700_ADDRESS, dataIn, 2);
+    return;
   }
+  //busy status bit EEbusy has to indicate, that the last transfer has been finished (EEbusy = 0)
+
+  //Before entering the command 11h, 12h, 21h or 22h, EECMD has to be written with 00h.
+  //Configuration EEPROM with RAM mirror, Address 2Bh and 30h to 37h: 35h EEPROM CLKOUT
+  //3.15.4. EEPROM CLKOUT REGISTER
 }
 
 void loop()
 {
+}
+
+bool writeRegister(byte reg, byte value)
+{
+  wire.beginTransmission(RV3028_ADDR);
+  byte data[2] = {reg, value};
+  return wire.writeEndTransmission(data, sizeof(data), true);
+}
+
+bool readRegister(byte reg, byte &value)
+{
+  wire.beginTransmission(RV3028_ADDR);
+  byte data[1];
+  if (!wire.writeEndTransmission(data, 1, false))
+  {
+    return false;
+  }
+  return wire.readRequestFrom(RV3028_ADDR, data, 1);
 }
